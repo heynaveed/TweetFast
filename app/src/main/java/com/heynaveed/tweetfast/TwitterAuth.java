795244b6432f4d9bea70362c906f233f1ec97a26
Naveed Shihab <heynaveed@gmail.com>
2016-12-15 13:20:31 +0000

    Initial commit

---
+++ app/src/main/java/com/heynaveed/tweetfast/TwitterAuth.java
@@ -0,0 +1,13 @@
+package com.heynaveed.tweetfast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TwitterAuth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_auth);
+    }
+}
