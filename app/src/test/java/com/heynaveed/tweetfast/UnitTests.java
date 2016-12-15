7cd0ca3a2cf94ae1e3e8e9682f22e96801c2851c
Heyna Veed <heynaveed@gmail.com>
2016-12-15 17:32:53 +0000

    Added new test to see if User object instantiation loads profile info username correctly.

--- app/src/test/java/com/heynaveed/tweetfast/UnitTests.java
+++ app/src/test/java/com/heynaveed/tweetfast/UnitTests.java
@@ -3,6 +3,7 @@ package com.heynaveed.tweetfast;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
+import com.heynaveed.tweetfast.entities.User;
 import com.heynaveed.tweetfast.utils.BearerToken;
 
 import org.junit.Test;
@@ -16,12 +17,30 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+@RequiresApi(api = Build.VERSION_CODES.KITKAT)
 public class UnitTests {
+
+    private static BearerToken token = null;
+
+    static {
+        try {
+            token = new BearerToken();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Test
     public void bearerToken_isCorrect() throws IOException {
-        String expected = "AAAAAAAAAAAAAAAAAAAAAHOWyQAAAAAAHwkJ3Ol8ircLJ718IRKF04GlNVs%3D93RhcbRWsIvGRcGuQVGJ7Aex0CDIJ9TeygwrLoVbfNrZuBCzG8";
-        assertEquals(expected, new BearerToken().getBearerToken());
+        final String expected = "AAAAAAAAAAAAAAAAAAAAAHOWyQAAAAAAHwkJ3Ol8ircLJ718IRKF04GlNVs%3D93RhcbRWsIvGRcGuQVGJ7Aex0CDIJ9TeygwrLoVbfNrZuBCzG8";
+        assertEquals(expected, token.getBearerToken());
+    }
+
+    @Test
+    public void user_loadsWithUsername() throws IOException {
+        final String username = "heynaveed";
+        User user = new User(username, token);
+        assertEquals(username, user.getUsername().toLowerCase());
     }
 }
