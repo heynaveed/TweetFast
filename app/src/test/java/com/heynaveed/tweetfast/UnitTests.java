b99397ff5c8a9313b2746dded414797844e19676
Heyna Veed <heynaveed@gmail.com>
2016-12-15 13:53:48 +0000

    New unit test on checking bearer token.

---
+++ app/src/test/java/com/heynaveed/tweetfast/UnitTests.java
@@ -0,0 +1,28 @@
+package com.heynaveed.tweetfast;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import com.heynaveed.tweetfast.utils.BearerToken;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTests {
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Test
+    public void bearerToken_isCorrect() throws IOException{
+        String expectedToken = "AAAAAAAAAAAAAAAAAAAAAHOWyQAAAAAAHwkJ3Ol8ircLJ718IRKF04GlNVs%3D93RhcbRWsIvGRcGuQVGJ7Aex0CDIJ9TeygwrLoVbfNrZuBCzG8";
+        BearerToken bToken = new BearerToken();
+        assertEquals(expectedToken, bToken.getBearerToken());
+    }
+}
