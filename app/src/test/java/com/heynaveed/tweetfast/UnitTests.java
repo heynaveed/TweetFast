dfd333ff5f68150a8ff42fab897eb558b450974b
Heyna Veed <heynaveed@gmail.com>
2016-12-15 17:28:29 +0000

    Added test to see if correct BearerToken is returned from Twitter API.

--- app/src/test/java/com/heynaveed/tweetfast/UnitTests.java
+++ app/src/test/java/com/heynaveed/tweetfast/UnitTests.java
@@ -3,6 +3,8 @@ package com.heynaveed.tweetfast;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 
+import com.heynaveed.tweetfast.utils.BearerToken;
+
 import org.junit.Test;
 
 import java.io.IOException;
@@ -16,4 +18,10 @@ import static org.junit.Assert.*;
  */
 public class UnitTests {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @Test
+    public void bearerToken_isCorrect() throws IOException {
+        String expected = "AAAAAAAAAAAAAAAAAAAAAHOWyQAAAAAAHwkJ3Ol8ircLJ718IRKF04GlNVs%3D93RhcbRWsIvGRcGuQVGJ7Aex0CDIJ9TeygwrLoVbfNrZuBCzG8";
+        assertEquals(expected, new BearerToken().getBearerToken());
+    }
 }
