5a9fe98dcbe75911a952ecc4c75aa9c1f686b86a
Heyna Veed <heynaveed@gmail.com>
2016-12-15 16:31:44 +0000

    Added new Colors class to hold all different ColorDrawable objects statically.

---
+++ app/src/main/java/com/heynaveed/tweetfast/utils/Colors.java
@@ -0,0 +1,12 @@
+package com.heynaveed.tweetfast.utils;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+/**
+ * Created by naveed.shihab on 15/12/2016.
+ */
+
+public class Colors {
+    public static final ColorDrawable TWITTER_BLUE = new ColorDrawable(Color.parseColor("#5c99fa"));
+}
