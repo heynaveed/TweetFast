cf9a37a6fbb1ae75cd0194253a7f32852bb61b4e
Heyna Veed <heynaveed@gmail.com>
2016-12-15 14:25:07 +0000

    Added Fabric API to project.

--- app/src/main/java/com/heynaveed/tweetfast/TwitterAuth.java
+++ app/src/main/java/com/heynaveed/tweetfast/TwitterAuth.java
@@ -2,12 +2,22 @@ package com.heynaveed.tweetfast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class TwitterAuth extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "o2psFcURWRf6y6ymutuweNLMw";
+    private static final String TWITTER_SECRET = "PasA4q4FM7xLDkmsqpJg5qcnxReQd9nozsewoMZuGIaFAPsFLi";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_twitter_auth);
     }
 }
